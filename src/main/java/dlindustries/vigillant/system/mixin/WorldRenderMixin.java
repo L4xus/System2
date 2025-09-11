2599c174847acad670cc710a8b48728236ff875c
L4xus <79592835+L4xus@users.noreply.github.com>
2025-09-11 12:33:20 +0300

    Update WorldRenderMixin.java

--- src/main/java/dlindustries/vigillant/system/mixin/WorldRenderMixin.java
+++ src/main/java/dlindustries/vigillant/system/mixin/WorldRenderMixin.java
@@ -1,12 +1,30 @@
+package dlindustries.vigillant.system.mixin;
+
+import dlindustries.vigillant.system.module.modules.render.ESP;
+import dlindustries.vigillant.system.system;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 @Mixin(WorldRenderer.class)
 public class WorldRenderMixin {
+    
     @Inject(method = "render", at = @At("HEAD"))
     private void onRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, 
                         Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, 
                         Matrix4f projectionMatrix, CallbackInfo ci) {
         
-        // Access through your system instance
+        // Get the ESP module from your module manager
         ESP espModule = system.INSTANCE.getModuleManager().getModule(ESP.class);
+        
+        // If ESP module exists and is enabled, call its render method
         if (espModule != null && espModule.isEnabled()) {
             espModule.onWorldRender(matrices, tickDelta);
         }
