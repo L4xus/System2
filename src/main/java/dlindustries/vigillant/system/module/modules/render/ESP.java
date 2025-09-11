93a71182a3685cd47b9bceba22eaaec6b7562a0c
L4xus <79592835+L4xus@users.noreply.github.com>
2025-09-11 12:25:29 +0300

    Update ESP.java

--- src/main/java/dlindustries/vigillant/system/module/modules/render/ESP.java
+++ src/main/java/dlindustries/vigillant/system/module/modules/render/ESP.java
@@ -2,122 +2,113 @@ package dlindustries.vigilant.system.module.modules.render;
 
 import dlindustries.vigilant.system.module.Category;
 import dlindustries.vigilant.system.module.Module;
-import dlindustries.vigilant.system.setting.Setting;
-import dlindustries.vigilant.system.setting.settings.BooleanSetting;
-import dlindustries.vigilant.system.setting.settings.ColorSetting;
-import dlindustries.vigilant.system.setting.settings.NumberSetting;
-import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import org.joml.Matrix4f;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import java.awt.*;
 
 public class ESP extends Module {
-    private final BooleanSetting players = new BooleanSetting("Players", this, true);
-    private final BooleanSetting mobs = new BooleanSetting("Mobs", this, false);
-    private final BooleanSetting animals = new BooleanSetting("Animals", this, false);
-    private final NumberSetting lineWidth = new NumberSetting("Line Width", this, 1.5, 0.5, 5.0, 0.1);
-    private final ColorSetting color = new ColorSetting("Color", this, new Color(255, 0, 0, 150));
+    private static ESP INSTANCE;
+    private final MinecraftClient mc = MinecraftClient.getInstance();
+    
+    public boolean players = true;
+    public boolean mobs = false;
+    public boolean animals = false;
+    public float lineWidth = 1.5f;
+    public Color color = new Color(255, 0, 0, 150);
     
     public ESP() {
         super("ESP", "See entities through walls", Category.RENDER);
-        
-        // Add settings
-        addSetting(players);
-        addSetting(mobs);
-        addSetting(animals);
-        addSetting(lineWidth);
-        addSetting(color);
+        INSTANCE = this;
     }
     
-    @SubscribeEvent
-    public void onRenderWorldLast(RenderWorldLastEvent event) {
+    public static ESP getInstance() {
+        return INSTANCE;
+    }
+    
+    public void onWorldRender(MatrixStack matrices, float tickDelta) {
         if (mc.world == null || mc.player == null || !isEnabled()) return;
         
-        // Store original GL state
-        GlStateManager.pushMatrix();
-        GlStateManager.disableTexture2D();
-        GlStateManager.disableDepth();
-        GlStateManager.disableLighting();
-        GlStateManager.enableBlend();
-        GlStateManager.blendFunc(770, 771);
-        GlStateManager.glLineWidth((float) lineWidth.getValue());
+        RenderSystem.disableDepthTest();
+        RenderSystem.disableCull();
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShader(GameRenderer::getRenderTypeLinesProgram);
+        RenderSystem.lineWidth(lineWidth);
         
-        // Set the color from settings
-        Color c = color.getValue();
-        GlStateManager.color(c.getRed() / 255f, c.getGreen() / 255f, c.getBlue() / 255f, c.getAlpha() / 255f);
+        RenderSystem.setShaderColor(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, color.getAlpha() / 255f);
         
-        // Render ESP for all entities
-        for (Entity entity : mc.world.loadedEntityList) {
+        for (Entity entity : mc.world.getEntities()) {
             if (shouldRenderEntity(entity)) {
-                renderESPBox(entity, event.getPartialTicks());
+                renderESPBox(entity, matrices, tickDelta);
             }
         }
         
-        // Restore original GL state
-        GlStateManager.popMatrix();
-        GlStateManager.enableTexture2D();
-        GlStateManager.enableDepth();
-        GlStateManager.enableLighting();
-        GlStateManager.disableBlend();
-        GlStateManager.color(1, 1, 1, 1);
+        RenderSystem.enableDepthTest();
+        RenderSystem.enableCull();
+        RenderSystem.disableBlend();
+        RenderSystem.setShaderColor(1, 1, 1, 1);
     }
     
     private boolean shouldRenderEntity(Entity entity) {
-        if (entity == mc.player) return false; // Don't render self
-        if (entity.isInvisible()) return false; // Skip invisible entities
+        if (entity == mc.player) return false;
+        if (entity.isInvisible()) return false;
         
-        if (entity instanceof EntityPlayer) {
-            return players.getValue();
+        if (entity instanceof PlayerEntity) {
+            return players;
         }
-        // You would add more entity type checks here for mobs, animals, etc.
-        // This is a simplified version
-        
         return false;
     }
     
-    private void renderESPBox(Entity entity, float partialTicks) {
-        // Calculate interpolated position
-        double x = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * partialTicks - mc.getRenderManager().viewerPosX;
-        double y = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * partialTicks - mc.getRenderManager().viewerPosY;
-        double z = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * partialTicks - mc.getRenderManager().viewerPosZ;
+    private void renderESPBox(Entity entity, MatrixStack matrices, float tickDelta) {
+        Vec3d cameraPos = mc.gameRenderer.getCamera().getPos();
+        double x = entity.prevX + (entity.getX() - entity.prevX) * tickDelta - cameraPos.x;
+        double y = entity.prevY + (entity.getY() - entity.prevY) * tickDelta - cameraPos.y;
+        double z = entity.prevZ + (entity.getZ() - entity.prevZ) * tickDelta - cameraPos.z;
         
-        // Entity dimensions
-        double width = entity.width / 2.0;
-        double height = entity.height;
+        Box box = entity.getBoundingBox().offset(-entity.getX(), -entity.getY(), -entity.getZ())
+                .offset(x, y, z);
         
-        // Draw the ESP box
-        GlStateManager.glBegin(3); // GL_LINE_STRIP
+        renderBoxOutline(matrices, box);
+    }
+    
+    private void renderBoxOutline(MatrixStack matrices, Box box) {
+        Matrix4f matrix = matrices.peek().getPositionMatrix();
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.getBuffer();
+        
+        buffer.begin(VertexFormat.DrawMode.LINES, VertexFormats.LINES);
         
         // Bottom rectangle
-        GlStateManager.glVertex3d(x - width, y, z - width);
-        GlStateManager.glVertex3d(x + width, y, z - width);
-        GlStateManager.glVertex3d(x + width, y, z + width);
-        GlStateManager.glVertex3d(x - width, y, z + width);
-        GlStateManager.glVertex3d(x - width, y, z - width);
+        addLine(buffer, matrix, box.minX, box.minY, box.minZ, box.maxX, box.minY, box.minZ);
+        addLine(buffer, matrix, box.maxX, box.minY, box.minZ, box.maxX, box.minY, box.maxZ);
+        addLine(buffer, matrix, box.maxX, box.minY, box.maxZ, box.minX, box.minY, box.maxZ);
+        addLine(buffer, matrix, box.minX, box.minY, box.maxZ, box.minX, box.minY, box.minZ);
         
         // Top rectangle
-        GlStateManager.glVertex3d(x - width, y + height, z - width);
-        GlStateManager.glVertex3d(x + width, y + height, z - width);
-        GlStateManager.glVertex3d(x + width, y + height, z + width);
-        GlStateManager.glVertex3d(x - width, y + height, z + width);
-        GlStateManager.glVertex3d(x - width, y + height, z - width);
+        addLine(buffer, matrix, box.minX, box.maxY, box.minZ, box.maxX, box.maxY, box.minZ);
+        addLine(buffer, matrix, box.maxX, box.maxY, box.minZ, box.maxX, box.maxY, box.maxZ);
+        addLine(buffer, matrix, box.maxX, box.maxY, box.maxZ, box.minX, box.maxY, box.maxZ);
+        addLine(buffer, matrix, box.minX, box.maxY, box.maxZ, box.minX, box.maxY, box.minZ);
         
         // Vertical lines
-        GlStateManager.glVertex3d(x - width, y, z - width);
-        GlStateManager.glVertex3d(x - width, y + height, z - width);
+        addLine(buffer, matrix, box.minX, box.minY, box.minZ, box.minX, box.maxY, box.minZ);
+        addLine(buffer, matrix, box.maxX, box.minY, box.minZ, box.maxX, box.maxY, box.minZ);
+        addLine(buffer, matrix, box.maxX, box.minY, box.maxZ, box.maxX, box.maxY, box.maxZ);
+        addLine(buffer, matrix, box.minX, box.minY, box.maxZ, box.minX, box.maxY, box.maxZ);
         
-        GlStateManager.glVertex3d(x + width, y, z - width);
-        GlStateManager.glVertex3d(x + width, y + height, z - width);
-        
-        GlStateManager.glVertex3d(x + width, y, z + width);
-        GlStateManager.glVertex3d(x + width, y + height, z + width);
-        
-        GlStateManager.glVertex3d(x - width, y, z + width);
-        GlStateManager.glVertex3d(x - width, y + height, z + width);
-        
-        GlStateManager.glEnd();
+        tessellator.draw();
+    }
+    
+    private void addLine(BufferBuilder buffer, Matrix4f matrix, double x1, double y1, double z1, double x2, double y2, double z2) {
+        buffer.vertex(matrix, (float)x1, (float)y1, (float)z1);
+        buffer.vertex(matrix, (float)x2, (float)y2, (float)z2);
     }
 }
